d8a47433dde3374a3972b1f53839fcecf7d65689
eddie <fangeddie@163.com>
2020-05-11 16:10:53 +0800

    style(base):增加logging说明

--- app_root/setting_diff/setting_base.py
+++ app_root/setting_diff/setting_base.py
@@ -204,6 +204,7 @@ LOGGING = {
 
     },
 }
+# 附录1：log等级
 # DEBUG:通常是生产环境调试消息
 # INFO:一般的系统消息
 # WARNING:发生了小问题
@@ -215,4 +216,17 @@ LOGGING = {
 # Filter 控制哪些日志可以从logger流向Handler
 # Handler 控制日志输出到哪里，console，file… 一个logger可以有多个Handler
 # Formatter 控制日志的格式
+
+# 附录2：日志格式
+# %(levelno)s: 打印日志级别的数值
+# %(levelname)s: 打印日志级别名称
+# %(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
+# %(filename)s: 打印当前执行程序名
+# %(funcName)s: 打印日志的当前函数
+# %(lineno)d: 打印日志的当前行号
+# %(asctime)s: 打印日志的时间
+# %(thread)d: 打印线程ID
+# %(threadName)s: 打印线程名称
+# %(process)d: 打印进程ID
+# %(message)s: 打印日志信息
 # ======== logging ========
