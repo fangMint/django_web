fa3bfeb67f98442674300f875c421dff59c964bf
eddie <fangeddie@163.com>
2020-05-11 16:07:37 +0800

    style(logging_test):增加脚本logging设置

---
+++ libs/logging_test.py
@@ -0,0 +1,15 @@
+# ==================================
+# Author   : fang
+# Time     : 2020/5/11 16:06
+# Email    : zhen.fang@qdreamer.com
+# File     : logging_test.py
+# Software : PyCharm
+# ==================================
+import logging
+"""
+logging 测试时输出第三方库的log
+"""
+
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
