1111ee58285af74936739db62d4e64a23ac00363
eddie <fangeddie@163.com>
2020-05-11 17:00:24 +0800

    style(-):

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source 虚拟环境/activate
 echo 'source active !'
-uwsgi --stop log/uwsgi.pid
+uwsgi --stop logs/uwsgi.pid
 echo 'uwsgi stop !'
 sleep 2s
 uwsgi --ini uwsgi.ini
 echo 'open log !'
-tail -f log/uwsgi.log
+tail -f logs/uwsgi.log
