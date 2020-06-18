0773f520669b78a5b45acb773022af3834d1c812
eddie <fangeddie@163.com>
2020-06-18 10:46:41 +0800

    feat(libs):更新返回值模板

--- libs/response_extra.py
+++ libs/response_extra.py
@@ -16,22 +16,6 @@ def response_failure(msg="", result=0, code=0, data=None):
     return response_template(msg, result, code, data)
 
 
-def al_does_not_exists(code=0):
-    return response_failure(msg="没有对应课程，没有binding？", code=code)
-
-
-def student_does_not_exists(code=0):
-    return response_failure(msg="没有对应学生", code=code)
-
-
-def face_does_not_exists(code=0):
-    return response_failure(msg="没有发现人像", code=code)
-
-
-def view_exception(code=0):
-    return response_failure(msg="网络不好", code=code)
-
-
 def msg_template(task, msg, result, code, data):
     return {
         "task": task, 'result': result, 'code': code,
@@ -45,3 +29,11 @@ def msg_success(task="", msg="", result=1, code=0, data=None):
 
 def msg_failure(task="", msg="", result=0, code=0, data=None):
     return msg_template(task, msg, result, code, data)
+
+
+def user_does_not_exists(code=0):
+    return response_failure(msg="没有对应用户", code=code)
+
+
+def view_exception(code=0):  # view出现异常
+    return response_failure(msg="网络不好", code=code)
